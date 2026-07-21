91148984a98fca2e526ddcc8616a8586051f56b9
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-07-21 21:28:27 +0530

    Time: 0 ms (100%), Space: 42 MB (72.08%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -16,16 +16,23 @@ public class Solution extends GuessGame {
         while(s<=e){
             int mid = s +(e-s)/2;
 
+            // guess ek method hai jo call krna padega  
+            //phele condition is num == to the computer is return the num , 0 means the computer guess and my guess ar equal
             if(guess(mid) == 0){
                 return mid ;  
             }
 
+            // if mera num less then aata hai computer ke num se toh mujhe right side jana hoga simple sa bs 
+            // yaha 1 is equal to the lower no. which we guess usse chota number 
             else if(guess(mid)==1){
                 
                s = mid+1;
 
             }
+            // -1 means humara num is greater then the computer vala no.
+            // so mujhe left me jake dekhna padega 
             else {
+                //guess(mid)==-1)
               e = mid-1;
             
             }
