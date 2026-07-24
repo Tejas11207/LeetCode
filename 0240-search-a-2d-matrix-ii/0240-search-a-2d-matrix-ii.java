65ba8ebb9f2c1262a293d8ebd3ea553b04b22bf4
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-07-24 21:47:39 +0530

    Time: 3 ms (89.19%), Space: 48.2 MB (64.82%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,25 +1,26 @@
+/// isme hum bs nhi use kr satke hai  kyu ki the prev element is bigger then the next row vala element and then brute froce me O(mXn ) tc aarha tha but this solution is more optimal then the brute force and m+n tc hai 
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int totalrow=matrix.length;
-        int totalcolumn= matrix[0].length;
+        int totalrow = matrix.length;
+        int totalcolumn = matrix[0].length;
 
         // mujhe chalue right most index se krna tha toh isliye ek duk corner vale ko pakad liya hai and humari condtion ko bhi satisfiya krna chaiye
         int row = 0;
-        int col = totalcolumn-1 ;
+        int col = totalcolumn - 1;
 
-        while (row <totalrow && col>=0){
-            if(matrix[row][col]==target){
-                return true ;
-            }
-            else if (matrix[row][col] > target ){
-              // col ko piche kro and then agge  apna element dhundo 
-                col -- ;
-            }
-            else {
+        while (row < totalrow && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                // col ko piche kro and then agge  apna element dhundo 
+                col--;
+            } else {
                 // matrix[row][col] < target
-                row ++ ; 
+                row++;
             }
         }
-        return false ;  
+        return false;
     }
 }
