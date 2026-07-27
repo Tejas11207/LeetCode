65578990ef054e936eca07fbf009dc598095b360
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-07-27 21:56:20 +0530

    Time: 5 ms (82.83%), Space: 77.5 MB (12.65%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxArea(int[] height) {
+        // two solve this question use two pointer approach 
+        int  n= height.length;
+        int left = 0;
+        int right = n-1;
+        int max = 0;
+
+        while(left <=right ){
+           // checks the min height of the array and line  or of the arrray
+            int h = Math.min(height[left],height[right]);
+
+            // widht
+            int width = right -left;
+            // max area occu. by the  water store
+            max = Math.max(max , width*h);
+
+            // agage piche kr raha hu pss 
+            if(height[left]< height[right]){
+                left ++;
+
+            }
+            else{
+                right --;
+            }
+
+        }
+        return max;
+    }
+}
