072da4324f1ac5534b7d511dd6b01798c21c9777
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-08-20 22:41:42 +0530

    Time: 0 ms (100%), Space: 48.1 MB (72.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -6,14 +6,20 @@ class Solution {
         int s= 0;
         int e = n-1;
 
+        //mujhe target ki fist and last index value chahiye 
+        // so isliye mene fist index ke liye create kiya hai varible
         int firstIndex =-1;
 
         while(s<=e){
 
             int mid = s +(e-s)/2;
 
+            // fir voh me bs 
             if(nums[mid]==target){
+                // mid mera target ke equal hai toh mid ke so mere first value voh hie vo gie
+
                 firstIndex = mid;
+                // then mene e ko shift kr diya kyuki most of the chance me mid ke aas paas hie meri value hogi
 
                 e = mid-1;
                 
@@ -32,6 +38,8 @@ class Solution {
         s = 0;
         e = n-1;
 
+        // same chize mene last index ke liye ki
+
         int lastIndex= -1;
 
         if(firstIndex==-1){
@@ -46,6 +54,7 @@ class Solution {
             if(nums[mid]==target){
                 lastIndex = mid ;
 
+                //bss mene abhi start index ko mid +1 kr diya
                 s =mid+1;
 
             }
