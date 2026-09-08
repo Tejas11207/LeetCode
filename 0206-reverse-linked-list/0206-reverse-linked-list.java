af37b0d773cee5774de1b579798245b50e1fdf3d
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-09-08 23:28:43 +0530

    Time: 0 ms (100%), Space: 44.4 MB (30.84%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -11,15 +11,15 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
 
-        ListNode prev =null;
+        ListNode last=null; // new null create kiya 
         ListNode curr = head ;
 
         while(curr != null){
                 ListNode temp = curr.next;  // step 1 ;
-            curr.next =prev;               // step 2
-            prev = curr;                   //step 3 
-            curr = temp ;                 // step 4
+            curr.next =last;   // attact to the null point            
+            last = curr;        // null ko current kr diya             //step 3 
+            curr = temp ;    // temp ko curr and temp ko curr.next              // step 4
         } 
-        return prev;
+        return last;  //last n vala prevoius vala 
     }
 }
