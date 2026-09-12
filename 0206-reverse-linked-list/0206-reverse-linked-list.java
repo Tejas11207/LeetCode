dba220e5a7680c125c3535551b724bd90e50bbc2
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-09-12 23:25:37 +0530

    Time: 0 ms (100%), Space: 44.1 MB (67.73%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,18 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+
+//         ListNode last=null; // new null create kiya 
+//         ListNode curr = head ;
+
+//         while(curr != null){
+//                 ListNode temp = curr.next;  // step 1 ;
+//             curr.next =last;   // attact to the null point            
+//             last = curr;        // null ko current kr diya             //step 3 
+//             curr = temp ;    // temp ko curr and temp ko curr.next              // step 4
+//         } 
+//         return last;  //last n vala prevoius vala 
+//     }
+// }
+
 class Solution {
-    public ListNode reverseList(ListNode head) {
 
-        ListNode last=null; // new null create kiya 
-        ListNode curr = head ;
+         public ListNode reverseList(ListNode head)
+         {
 
-        while(curr != null){
-                ListNode temp = curr.next;  // step 1 ;
-            curr.next =last;   // attact to the null point            
-            last = curr;        // null ko current kr diya             //step 3 
-            curr = temp ;    // temp ko curr and temp ko curr.next              // step 4
-        } 
-        return last;  //last n vala prevoius vala 
-    }
+            ListNode  prev = null;
+            ListNode  curr = head;
+
+            while(curr !=null){
+
+                ListNode forward = curr.next;
+
+                curr.next = prev;
+                prev= curr;
+                curr= forward;
+
+            } 
+            return prev;
+         }
+
 }
