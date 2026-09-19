f9bb8bc50d60cde5a9c0d1e673c482ef0a6bbfc4
Tejas Joshi <tejasjoshi0712@gmail.com>
2026-09-19 22:14:28 +0530

    Time: 0 ms (100%), Space: 45 MB (79.41%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -9,9 +9,41 @@
 class Solution {
     public void deleteNode(ListNode node) {
 
-        node.val = node.next.val;
-        node.next = node.next.next;
+        // ListNode  prev = node ;
+        // ListNode curr = node ;
 
+        // ListNode  forward = curr.next ;
+
+        // for(int i =1 ; i<=node -2 ;i++){
+
+        //     curr = curr.next;
+
+        // }
+
+        // prev.next = forward ;
+
+        // curr.next= null;
+
+        //return node
+
+        // values. ko copy krnaa 
+        // eg 4->5->1->9->null.  delete 5
+
+
+        node.val = node.next.val;  // duplicate value create  4->1->1->9->null. // 5 ko  memory se hata diya 
+        // bawkuff bana diya 
+        // same value create ki then humne next same value value ko delete kra 
+
+        node.next = node.next.next; // then link change  node.next = node.next.next  // link change 
+
+
+
+
+
+
+
+        
+      
         
     }
 }
